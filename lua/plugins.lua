e5489468c73175d2ee09333d4c44b37afe2ade9c
FvD <FvD@users.noreply.github.com>
2022-01-13 15:26:09 -0600

    auto install packer

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,3 +1,10 @@
+-- Bootstrap packer if not available
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
 return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
 
@@ -83,4 +90,9 @@ return require('packer').startup(function()
   -- search
   use { 'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}} }
 
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
